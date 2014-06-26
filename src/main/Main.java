901a35936677aa35a247ec656dcc7f6ab65c0dee
thomasdashney <tdashney@gmail.com>
2014-06-25 22:16:49 -0400

    basic command line working

---
+++ src/main/Main.java
@@ -0,0 +1,30 @@
+package main;
+
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+public class Main {
+	public static void main(String[] args) {
+		Options options = new Options();
+		options.addOption("t", false, "test flag 't'");
+		
+		CommandLineParser parser = new BasicParser();
+		CommandLine cmd = null;
+		try {
+			cmd = parser.parse(options, args);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		if( cmd.hasOption("t") ) {
+		    // initialise the member variable
+		    System.out.println("flag!");
+		}
+		else {
+			System.out.println("no flag!");
+		}
+	}
+}
